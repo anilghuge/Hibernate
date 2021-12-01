24b3984c9cea96eb9c9916a91b29ee2a7d871d38
Anil <anilghuge780@gmail.com>
2021-12-01 20:14:12 +0530

    hibernate

---
+++ autoDDL/src/main/java/bean/Course.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class Course {
+
+	private int cid;
+	private String cname;
+	private int cfee;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public int getCfee() {
+		return cfee;
+	}
+	public void setCfee(int cfee) {
+		this.cfee = cfee;
+	}
+	
+}
